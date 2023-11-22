2573e8a4d19ea1afb9492b8868e3f2dff3bd9702
Kloss <108338208+MosesAdaeze@users.noreply.github.com>
2023-11-22 19:14:04 +0100

    commited
    Javascript.

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ let header = document.querySelector('header');
 window.addEventListener('scroll', () => {
     header.classList.toggle('shadow', window.scrollY > 0);
 });
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .nav li a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header .nav li a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
